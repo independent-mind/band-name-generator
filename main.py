08faa1902ae705bdd586f51064644a09ba973c28
independent-min <46705473+independent-mind@users.noreply.github.com>
2022-11-19 21:40:06 +0000

    minor corrections

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print("Welcome to Band name generator.")
-city = input("Whend did you live as a child (city)?\n")
+city = input("Where did you live as a child (what city)?\n")
 pet = input("What's your pet's name?\n")
 bandName = city + " " + pet
 print(f"Your band name could be: {bandName}")
